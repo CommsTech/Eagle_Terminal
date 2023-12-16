1ed99b47d37d98d8677444f0e43a42e72970e451
CommsTech <commstech@commsnet.org>
2023-12-16 16:55:07 -0500

    code updates and reworks
    pack -> grid conversion #for more complex UI ! note failed and reverted back... im still learning
    
    fixed the add custom button commands
    
    removed manual SSH and Serial Connection tabs
    
    username and password is not required if keys are utilized. keys are held in the .ssh directory
    
    Added network status indicator... does not dynamically update

--- Eagle_Terminal.py
+++ Eagle_Terminal.py
@@ -1,7 +1,9 @@
 import csv
 import subprocess
 import sys
+import logging
 import tkinter as tk
+import device_status
 from tkinter import ttk, messagebox, Scrollbar, Text, Entry
 try:
     import paramiko
@@ -13,6 +15,11 @@ except ImportError:
 from serial.tools.list_ports import comports
 from functools import partial
 
+#logging.basicConfig(level=logging.DEBUG)
+
+#define the commands file for quick access
+commands_file = 'commands.txt'
+
 class ConnectionTab:
     def __init__(self, parent, notebook):
         self.parent = notebook
@@ -23,6 +30,7 @@ class ConnectionTab:
         self.create_userpass_widgets()
         self.create_ssh_widgets()
         self.create_serial_widgets()
+        self.create_configure_button()
 
     def create_userpass_widgets(self):
         self.username_label = ttk.Label(self.tab, text="Username:")
@@ -49,19 +57,20 @@ class ConnectionTab:
             return []
 
     def create_ssh_widgets(self):
+        # Get the status for the current device
         for device in self.devices:
             # Create the device widget
             device_widget = ttk.Frame(self.tab)
             device_widget.pack()
-
             device_label = device['device_label']
             connection_type = device['connection_type']
             host = device['host']
+            status = device_status.get_status(host)
 
             if connection_type == 'SSH':
-                button_text = f"{device_label} - {connection_type} {host}"
-                self.create_button(self.tab, button_text, self.select_connection, connection_type=connection_type, host=host)
-
+                button_text = f"{device_label} - {connection_type} {host} ({status['status']})"
+                button = self.create_button(self.tab, button_text, self.select_connection, connection_type=connection_type, host=host)
+        
     def create_serial_widgets(self):
         for row in self.devices:
             device_label = row['device_label']
@@ -101,22 +110,82 @@ class ConnectionTab:
         
             def send_command(output_text, command_entry, ssh_client):
                 command = command_entry.get()
-                while command:
-                    stdin, stdout, stderr = ssh_client.exec_command(command)
-                    output = stdout.read().decode("utf-8")
-                    output_text.insert(tk.END, output)
-                    command_entry.delete(0, tk.END)
-                    command = command_entry.get()
+                if not command:
+                    return
+            
+                stdin, stdout, stderr = ssh_client.exec_command(command)
+                # Read the output in chunks
+                output = ""
+                for line in stdout:
+                    output += line.strip() + "\n"
+                for line in stderr:
+                    output += line.strip() + "\n"
+                    
+                output_text.insert(tk.END, output)
+                output_text.see(tk.END)
+
+                command_entry.delete(0, tk.END)
+                command_entry.focus_set()
         
             command_entry.bind("<Return>", lambda event: send_command(output_text, command_entry, ssh_client))
             self.parent.mainloop()
             ssh_client.close()
         else:
             print("Invalid connection type")
+    def create_configure_button(self):
+        configure_button = ttk.Button(self.tab, text="Configure Custom Connection", command=self.configure_custom_connection)
+        configure_button.pack()
 
     def configure_custom_connection(self):
         custom_window = tk.Toplevel(self.parent)
         custom_window.title("Custom Connection")
+    
+        # Create and pack the labels and entry fields
+        ttk.Label(custom_window, text="Device Label:").pack()
+        device_label_entry = ttk.Entry(custom_window)
+        device_label_entry.pack()
+    
+        ttk.Label(custom_window, text="Connection Type:").pack()
+        connection_type_var = tk.StringVar(value="SSH")
+        connection_type_dropdown = ttk.Combobox(custom_window, textvariable=connection_type_var, values=["SSH", "Serial"])
+        connection_type_dropdown.pack()
+    
+        ttk.Label(custom_window, text="Host:").pack()
+        host_entry = ttk.Entry(custom_window)
+        host_entry.pack()
+    
+        # Function to save the device information
+        def save_device():
+            device_label = device_label_entry.get()
+            connection_type = connection_type_var.get()
+            host = host_entry.get()
+    
+            # Append the device information to the devices list
+            self.devices.append({
+                'device_label': device_label,
+                'connection_type': connection_type,
+                'host': host
+            })
+    
+            # Create the device button
+            button_text = f"{device_label} - {connection_type} {host}"
+            self.create_button(self.tab, button_text, self.select_connection, connection_type=connection_type, host=host)
+            
+            # append the device information to the devices.csv file
+            with open('devices.csv', 'a', newline='') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=['device_label', 'connection_type', 'host'])
+                writer.writerow({
+                    'device_label': device_label,
+                    'connection_type': connection_type,
+                    'host': host
+                })
+            # Close the custom window
+            custom_window.destroy()
+    
+        # Create and pack the save button
+        save_button = ttk.Button(custom_window, text="Save", command=save_device)
+        save_button.pack()
+    
         custom_window.mainloop()
 
     def set_username_placeholder(self, event):
@@ -128,153 +197,6 @@ class ConnectionTab:
             self.password_entry.insert(0, "Password")
 
 
-class SSHTab:  
-    def __init__(self, parent):
-        self.parent = parent
-        self.tab = ttk.Frame(self.parent)
-        self.parent.add(self.tab, text="SSH")
-        entries = [
-            ("Host:", ttk.Entry),
-            ("Username:", ttk.Entry),
-            ("Password:", partial(ttk.Entry, show="*"))
-        ]
-        for label, entry_constructor in entries:
-            ttk.Label(self.tab, text=label).pack()
-            entry_constructor(self.tab).pack()
-    
-        self.connect_button = ttk.Button(self.tab, text="Connect", command=self.connect_ssh)
-        self.connect_button.pack()
-        self.close_button = ttk.Button(self.tab, text="X", command=self.close_tab)
-        self.close_button.pack(side="right")
-        
-    def connect_ssh(self):
-        host = self.host_entry.get()
-        username = self.user_entry.get()
-        password = self.password_entry.get()
-        ssh_client = paramiko.SSHClient()
-        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(host, username=username, password=password)
-        ssh_session_tab = ttk.Frame(self.parent)
-        self.parent.add(ssh_session_tab, text=f"SSH - {host}")
-        ssh_output = Text(ssh_session_tab)
-        ssh_output.pack(fill="both", expand=True)
-        scrollbar = ttk.Scrollbar(ssh_session_tab, command=ssh_output.yview)
-        scrollbar.pack(side="right", fill="y")
-        ssh_output.configure(yscrollcommand=scrollbar.set)
-        channel = ssh_client.invoke_shell()
-    
-        def send_input(event):
-            input_text = input_entry.get()
-            ssh_channel.send(input_text + "\n")
-            input_entry.delete(0, tk.END)
-    
-        ssh_input = Entry(ssh_session_tab)
-        ssh_input.pack(fill="x")
-        ssh_input.bind("<Return>", send_input)
-    
-        def receive_output():
-            if channel.recv_ready():
-                output = channel.recv(1024).decode()
-                ssh_output.insert("end", output)
-                ssh_output.see("end")
-            ssh_output.after(100, receive_output)
-        receive_output()
-    
-        def close_ssh_tab():
-            ssh_client.close()
-    
-        close_button = ttk.Button(ssh_session_tab, text="Close", command=close_ssh_tab)
-        close_button.pack()
-    def close_tab(self):
-        pass
-
-class SerialTab:
-    
-    def __init__(self, parent):
-        self.parent = parent
-        self.tab = ttk.Frame(self.parent)
-        self.parent.add(self.tab, text="Serial")
-        self.create_port_widgets()
-        self.create_baud_widgets()
-        self.create_connect_button()
-        
-    def create_port_widgets(self):
-        self.port_label = ttk.Label(self.tab, text="Port:")
-        self.port_label.grid(row=0, column=0, sticky="w")
-        self.port_combobox = ttk.Combobox(self.tab, values=[port.device for port in comports()])
-        self.port_combobox.grid(row=0, column=1, sticky="w")
-    
-    def create_baud_widgets(self):
-        self.baud_label = ttk.Label(self.tab, text="Baud Rate:")
-        self.baud_label.grid(row=1, column=0, sticky="w")
-        self.baud_entry = ttk.Entry(self.tab)
-        self.baud_entry.grid(row=1, column=1, sticky="w")
-    
-    def create_connect_button(self):
-        self.connect_button = ttk.Button(self.tab, text="Connect", command=self.connect_serial)
-        self.connect_button.grid(row=2, column=0, columnspan=2, sticky="w")
-        
-    def connect_serial(self):
-        port = self.port_combobox.get()
-        baud_rate = int(self.baud_entry.get())
-        
-        try:
-            ser = serial.Serial(port, baud_rate)
-            # Code to handle the established serial connection
-            print("Serial connection established successfully.")
-            
-            # Create a new tab for the Serial session
-            serial_session_tab = ttk.Frame(self.parent)
-            self.parent.add(serial_session_tab, text=f"Serial - {port}")
-            
-            # Create a Text widget to display terminal-like output
-            serial_output = Text(serial_session_tab)
-            serial_output.pack(fill="both", expand=True)
-            
-            # Create a Scrollbar for the Text widget
-            scrollbar = ttk.Scrollbar(serial_session_tab, command=serial_output.yview)
-            scrollbar.pack(side="right", fill="y")
-            
-            # Configure the Text widget to use the Scrollbar
-            serial_output.configure(yscrollcommand=scrollbar.set)
-            
-            # Function to send command
-            def send_command(event):
-                command = serial_input.get()
-                ser.write(command.encode() + b'\n')
-                serial_input.delete("1.0", "end")
-            
-            # Create an Entry widget to input commands
-            serial_input = Entry(serial_session_tab)
-            serial_input.pack(fill="x")
-            serial_input.bind("<Return>", send_command)
-            
-            # Function to receive output
-            def receive_output():
-                output = ser.readline().decode()
-                serial_output.insert("end", output)
-                serial_output.see("end")
-                serial_output.after(100, receive_output)
-            
-            # Start receiving output
-            receive_output()
-            
-            # Perform serial communication...
-            # e.g. read/write data to the device
-            
-            # Close the Serial connection when the tab is closed
-            def close_serial_tab():
-                ser.close()
-            
-            # Add a close button to the tab
-            close_button = ttk.Button(serial_session_tab, text="Close", command=close_serial_tab)
-            close_button.pack()
-            
-        except serial.SerialException as e:
-            print("Error: Failed to establish serial connection:", str(e))
-    
-    def close_tab(self):
-        pass
 
 if __name__ == "__main__":
     # Create the main application window
@@ -295,14 +217,14 @@ if __name__ == "__main__":
 
     # Create the tabs
     connection_tab = ConnectionTab(root, notebook)
-    ssh_tab = SSHTab(notebook)
-    serial_tab = SerialTab(notebook)
+    #ssh_tab = SSHTab(notebook)
+    #serial_tab = SerialTab(notebook)
     
 
     # Add the tabs to the notebook
     notebook.add(connection_tab.tab, text="Connections")
-    notebook.add(ssh_tab.tab, text="SSH")
-    notebook.add(serial_tab.tab, text="Serial")
+    #notebook.add(ssh_tab.tab, text="SSH")
+    #notebook.add(serial_tab.tab, text="Serial")
     
 
     root.mainloop()
---
+++ device_status.py
@@ -0,0 +1,71 @@
+import subprocess
+import csv
+import threading
+import time
+
+def get_status(ip_address):
+    status = {
+        'device_label': None,
+        'connection_type': None,
+        'status': 'pending'  # Initial pending status
+    }
+
+    def update_status():
+        while True:
+            # Retrieve the real status here (e.g., by calling get_network_status)
+            real_status = get_network_status(ip_address)
+            status['status'] = real_status
+            time.sleep(120) # wait for 120 seconds before updating again
+
+    with open('devices.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
+
+        for row in csv_reader:
+            if len(row) < 3:
+                continue  # Skip the line if it doesn't have enough columns
+
+            device_label = row[0]
+            connection_type = row[1]
+            host = row[2]
+
+            if connection_type == "Serial" or not host:
+                continue  # Skip the line if connection_type is "Serial" or host is blank
+
+            if host == ip_address:
+                status['device_label'] = device_label
+                status['connection_type'] = connection_type
+
+                # Start a separate thread to update the status asynchronously
+                update_thread = threading.Thread(target=update_status)
+                update_thread.start()
+
+    return status
+
+def get_network_status(ip_address):
+    with open('devices.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
+
+        for row in csv_reader:
+            device_label = row[0]
+            connection_type = row[1]
+            host = row[2]
+
+            # Perform ping operation
+            ping_result = subprocess.run(['ping', '-n', '1', '-w', '1000', ip_address], capture_output=True, text=True)
+            if ping_result.returncode == 0:
+                return 'Online'  # Return 'Online' if ping operation is successful
+            else:
+                return 'Offline'
+
+            # Perform traceroute operation
+            # traceroute_result = subprocess.run(['traceroute', host], capture_output=True, text=True)
+            # traceroute_output = traceroute_result.stdout
+
+            # Process the ping output and traceroute output as needed
+            # ...
+
+            # Save the results to a file or database
+            # ...
+    pass
